0096e2c3d3c644084f618e53037946cc0f285ab3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-18 12:06:35 +0000

    Deploy aziza-calm/2019-2-Track-Frontend-A-Kalmasova to github.com/aziza-calm/2019-2-Track-Frontend-A-Kalmasova.git:gh-pages

---
+++ precache-manifest.6c9284f812e84adb9ff5364431713021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0c9eb5274763d811be032cb865ef54",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/index.html"
+  },
+  {
+    "revision": "4a97f775edb329bf2523",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/static/css/main.b4eae362.chunk.css"
+  },
+  {
+    "revision": "1cb8ca2fb52a6508df4a",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/static/js/2.fc796bdb.chunk.js"
+  },
+  {
+    "revision": "4a97f775edb329bf2523",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/static/js/main.4c058e5a.chunk.js"
+  },
+  {
+    "revision": "aa3304ac825762c665ac",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/static/js/runtime-main.73197591.js"
+  },
+  {
+    "revision": "795fb555bea801821f51202dc10aaa78",
+    "url": "/2019-2-Track-Frontend-A-Kalmasova/static/media/logo.795fb555.svg"
+  }
+]);
